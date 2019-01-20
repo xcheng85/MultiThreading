b393346dd9ba4c2a4a94edb1c8c819649986deac
xiao cheng <xcheng4@xiaos-MacBook-Pro.local>
2019-01-20 12:48:39 -0600

    add a first c++ threads example with cmake flag to compile

---
+++ cppThreads/src/cpp_threads.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+void accumulator_function2(const std::vector<int> &v, unsigned long long &acm, 
+                            unsigned int beginIndex, unsigned int endIndex)
+{
+    acm = 0;
+    for (int i = beginIndex; i < endIndex; ++i)
+    {
+        acm += v[i];
+    }
+}
+
+int main()
+{
+    unsigned long long acm1 = 0;
+    unsigned long long acm2 = 0;
+
+    vector<int> v(100, 0); 
+    for(int i = 0; i < 100; i++){
+        v[i] = i; 
+    }
+
+    std::thread t1(accumulator_function2, std::ref(v), std::ref(acm1), 0, v.size() / 2);
+    std::thread t2(accumulator_function2, std::ref(v), std::ref(acm2), v.size() / 2, v.size());
+    t1.join();
+    t2.join();
+
+    std::cout << "acm1: " << acm1 << endl;
+    std::cout << "acm2: " << acm2 << endl;
+    std::cout << "acm1 + acm2: " << acm1 + acm2 << endl;
+}
--- readme.txt
+++ readme.txt
@@ -2,5 +2,17 @@ git remote add hellosvc https://github.com/xcheng85/hellosvc.git
 git push --set-upstream hellosvc master
 
 
+# Run task to cmake 
+
 # Run build task once having code changes 
 
+# Run make install to copy exc to expected directory
+
+
+Modern c++ 
+c++11: Multithreading, memory model
+c++14: read-write lock
+c++17: parallel stl
+c++20: new atomic smart pointer, futures, semophore()
+
+add flag in CMakeList.txt (project level)
